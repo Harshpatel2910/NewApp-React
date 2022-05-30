b9cb03a9cec3cc31ca8027041b7f122576632a0a
Harshpatel2910 <hppatel530@gmail.com>
2022-05-30 22:38:21 +0530

    sample news updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
+import NewsItem from './components/NewsItem';
 
 export default class App extends Component {
   render() {
     return (
-      <div>App </div>
+      <div>
+      
+        <NavBar></NavBar>
+
+        <News></News>
+       
+      </div>
     )
   }
 }
