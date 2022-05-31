7680f0d2da67bf322f9fc177beb999b44920f0c8
Harshpatel2910 <hppatel530@gmail.com>
2022-05-31 23:31:07 +0530

    category wise news added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
-import NewsItem from './components/NewsItem';
+
 
 
 export default class App extends Component {
   render() {
     return (
       <div>
-      
+
         <NavBar></NavBar>
 
-        <News pageSize={5} country="in" catogary="science"></News>
-       
+        <News pageSize={5} country="in" category="sports"></News>
+
       </div>
     )
   }
