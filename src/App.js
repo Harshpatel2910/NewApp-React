c390568bdc1ac6c0aa7bb2728ceea7b7785c334f
Harshpatel2910 <hppatel530@gmail.com>
2022-06-03 00:48:27 +0530

    loading bar updated and app is completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
 import LoadingBar from 'react-top-loading-bar'
@@ -11,34 +11,37 @@ import {
 } from 'react-router-dom';
 
 
-
-export default class App extends Component {
-  pageSize = 5;
-  render() {
+const App = ()=> {
+  const pageSize = 5;
+  const apiKey = process.env.REACT_APP_NEWS_API
+  const [progress, setProgress] = useState(0)
+ 
+ 
     return (
       <>
         <BrowserRouter>
           <NavBar></NavBar>
           <LoadingBar
-        height={3}
-        color='#f11946'
-        progress={this.state.progress} 
-      />
+            height={3}
+            color='#f11946'
+            progress={progress}
+          />
           <Routes>
-             
-           
-            <Route path="/" element={<News key="general"pageSize={this.pageSize} country="in" category="general"></News>} /> 
-            <Route path="/general" element={<News key="general" pageSize={this.pageSize} country="in" category="general"></News>} />
-            <Route path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"></News>} />
-            <Route path="/health" element={<News key="health" pageSize={this.pageSize} country="in" category="health"></News>} />
-            <Route path="/science" element={<News key="science" pageSize={this.pageSize} country="in" category="science"></News>} />
-            <Route path="/technology" element={<News key="technology" pageSize={this.pageSize} country="in" category="technology"></News>} />
-            <Route path="/business" element={<News key="business" pageSize={this.pageSize} country="in" category="business"></News>} />
-            <Route path="/sports" element={<News key="sports" pageSize={this.pageSize} country="in" category="sports"></News>} />
+
+
+            <Route path="/" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"></News>} />
+            <Route path="/general" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"></News>} />
+            <Route path="/entertainment" element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment"></News>} />
+            <Route path="/health" element={<News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health"></News>} />
+            <Route path="/science" element={<News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science"></News>} />
+            <Route path="/technology" element={<News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology"></News>} />
+            <Route path="/business" element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business"></News>} />
+            <Route path="/sports" element={<News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports"></News>} />
 
           </Routes>
         </BrowserRouter>
       </>
     )
-  }
+  
 }
+export default App;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,34 +1,37 @@
-import React, { Component } from 'react'
+import React from 'react'
+const NewsItem = (props) => {
 
-export class NewsItem extends Component {
+    let { title, description, imageUrl, url, author, date, source } = props;
 
+    return (
 
+        <>
+            <div className="my-3">
+                <div className="card">
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position: 'absolute',
+                        right: '0'
+                    }
+                    }>
 
-    render() {
-
-        let { title, description, imageUrl, url,author,date,source } = this.props;
-
-        return (
-
-            <>
-                <div className='my-3'>
-                    <div className="card">
- 
-                    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}> {source}
+                        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '90%', zIndex: '1' }}> {source}
                         </span>
-                        <img src={imageUrl} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">{title}...</h5>
-                            <p className="card-text">{description}...</p>
-                            <p className="card-text"><small className='text-muted'>By {author} on {new Date(date).toGMTString()}</small></p>
-                            <a href={url} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark">Read More</a>
-                        </div>
+                    </div>
+                    <img src={imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}...</p>
+                        <p className="card-text"><small className='text-muted'>By {author} on {new Date(date).toGMTString()}</small></p>
+                        <a href={url} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
+            </div>
 
-            </>
-        )
-    }
+        </>
+    )
+
 }
 
 export default NewsItem
