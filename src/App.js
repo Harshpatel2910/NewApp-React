c8388d64e5eb4698cd576ad5b69ec0f431f0b5a4
Harshpatel2910 <hppatel530@gmail.com>
2022-06-02 21:33:36 +0530

    infinite scroll bar updated

--- src/App.js
+++ src/App.js
@@ -3,19 +3,42 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+import LoadingBar from 'react-top-loading-bar'
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from 'react-router-dom';
 
 
 
 export default class App extends Component {
+  pageSize = 5;
   render() {
     return (
-      <div>
+      <>
+        <BrowserRouter>
+          <NavBar></NavBar>
+          <LoadingBar
+        height={3}
+        color='#f11946'
+        progress={this.state.progress} 
+      />
+          <Routes>
+             
+           
+            <Route path="/" element={<News key="general"pageSize={this.pageSize} country="in" category="general"></News>} /> 
+            <Route path="/general" element={<News key="general" pageSize={this.pageSize} country="in" category="general"></News>} />
+            <Route path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"></News>} />
+            <Route path="/health" element={<News key="health" pageSize={this.pageSize} country="in" category="health"></News>} />
+            <Route path="/science" element={<News key="science" pageSize={this.pageSize} country="in" category="science"></News>} />
+            <Route path="/technology" element={<News key="technology" pageSize={this.pageSize} country="in" category="technology"></News>} />
+            <Route path="/business" element={<News key="business" pageSize={this.pageSize} country="in" category="business"></News>} />
+            <Route path="/sports" element={<News key="sports" pageSize={this.pageSize} country="in" category="sports"></News>} />
 
-        <NavBar></NavBar>
-
-        <News pageSize={5} country="in" category="sports"></News>
-
-      </div>
+          </Routes>
+        </BrowserRouter>
+      </>
     )
   }
 }
